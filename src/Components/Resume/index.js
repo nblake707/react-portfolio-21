7bb4bb9b849f0ab984aeddcdda0f75395dc814ce
nblake707 <33844526+nblake707@users.noreply.github.com>
2021-12-20 21:59:53 -0500

    update

---
+++ src/Components/Resume/index.js
@@ -0,0 +1,41 @@
+import { Container, Row, Col, Form } from "react-bootstrap";
+
+const Resume = () => {
+  return (
+    <Container>
+        <Row>
+            <Col>
+            <h1>Resume</h1>
+            </Col>   
+        </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Resume;
